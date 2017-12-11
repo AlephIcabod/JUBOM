0f01054148a37a909157f1aaaab02556c57ee0b1
Miguel Angel Bautista Cruz <angel_507@live.com.mx>
2017-12-10 19:15:29 -0600

    node start

--- public/js/fb.js
+++ public/js/fb.js
@@ -62,7 +62,7 @@ function postLike(msg) {
     mobile_iframe: true,
     display:"touch",    
     //href: 'https://developers.facebook.com/docs/',
-    action_properties:JSON.stringify({object:"jubom.herokuapp.com",start_time:new Date()})
+    action_properties:JSON.stringify({object:"https://jubom.herokuapp.com",start_time:new Date()})
   }, function(response){
 
     console.log(response)
